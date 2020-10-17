81f2a77576c9a215fa4ceb9814a4e0be22f28c5a
Sourabh Kulkarni <sourabhuk254@gmail.com>
2020-10-17 18:14:23 -0400

    Configure multiple checkboxes by prop

--- projects/react-components/src/stories/Checkbox.stories.js
+++ projects/react-components/src/stories/Checkbox.stories.js
@@ -1,19 +1,17 @@
-import React, { useState, useContext } from 'react'
-import { Checkbox } from '../components'
-import {
-  VelocityThemeProvider,
-  VelocityThemeContext,
-} from '../utils/VelocityThemeContext'
+import React from 'react'
+import {Checkbox} from '../components'
+import {VelocityThemeProvider,} from '../utils/VelocityThemeContext'
 
 export default {
-  title: 'Checkbox',
-  component: Checkbox,
+    title: 'Checkbox',
+    component: Checkbox,
 }
 export const Component = () => {
-  return (
-    <VelocityThemeProvider>
-      <Checkbox label={['My Checkbox']} />
-      <Checkbox label={['My Checkbox1', 'My Checkbox2', 'My Checkbox3']} />
-    </VelocityThemeProvider>
-  )
+    return (
+        <>
+            <VelocityThemeProvider>
+                <Checkbox label={['My Checkbox1', 'My Checkbox2', 'My Checkbox3']}/>
+            </VelocityThemeProvider>
+        </>
+    )
 }
