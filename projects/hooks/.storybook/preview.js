3e0b667efc3682694550cca87352bb4572033b40
brandon-schabel <brandonschabel1995@gmail.com>
2020-11-08 14:16:59 -0700

    add hooks project

---
+++ projects/hooks/.storybook/preview.js
@@ -0,0 +1,6 @@
+// import React from "react";
+// import { VelocityThemeProvider } from "../src/utils/VelocityThemeContext";
+//
+// export const decorators = [
+//   Story =>
+// ];
---
+++ projects/hooks/src/utils/generateKey.js
@@ -0,0 +1,9 @@
+import { getRandInt } from './getRandInt'
+
+/**
+ *
+ * @param pre
+ * @returns {string}
+ */
+export const generateKey = pre =>
+  `${pre ? pre : getRandInt(0, 200)}_${new Date().getTime()}`
